b419467121c05112283c118798f24523e5de3169
TheFather <TheFather@KUARLSLAND.local>
2023-02-12 22:01:20 +0100

    proyectProgramacion

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,66 @@
+let ataqueJugador
+
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
+}
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
+
+    if(inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = "Hipodoge"
+    } 
+    else if(inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = "Capipepo"
+    }
+    else if(inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = "Ratigueya"
+    }
+    else {
+        alert("SELECCIONA UNA MASCOTA!!!!!!!")
+    }
+    seleccionarMascotaEnemigo()
+}
+
+
+
+function seleccionarMascotaEnemigo() {
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "Hipodoge"
+    }else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = "Capipepo"
+    }else {
+        spanMascotaEnemigo.innerHTML = "Ratigueya"
+    }
+}
+
+function ataqueFuego(){
+    ataqueJugador = "FUEGO"
+    alert(ataqueJugador)
+}
+function ataqueAgua(){
+    ataqueJugador = "AGUA"
+    alert(ataqueJugador)
+}
+function ataqueTierra(){
+    ataqueJugador = "TIERRA"
+    alert(ataqueJugador)
+}
+
+function aleatorio(min, max){ return Math.floor(Math.random() * (max - min + 1) + min)
+
+}
+window.addEventListener("load", iniciarJuego)
